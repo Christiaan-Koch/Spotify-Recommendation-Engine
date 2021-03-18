7b1a8018fd2809ccf3e5d162aaa2d4b72ddb6fc2
ChristiaanKoch <christiaan.koch@entelect.co.za>
2021-03-18 14:54:18 +0200

    Added database drop script

---
+++ database/DatabaseDropScript.sql
@@ -0,0 +1,8 @@
+DROP TABLE SpotifyRecommendationEngine.dbo.IncludedContent;
+DROP TABLE SpotifyRecommendationEngine.dbo.ExcludedContent;
+DROP TABLE SpotifyRecommendationEngine.dbo.ContentType;
+DROP TABLE SpotifyRecommendationEngine.dbo.SpotifyUser;
+GO
+
+DROP DATABASE SpotifyRecommendationEngine
+GO
