8c6d4f520fe032dcf78d2d91809397af0a510d39
Christiaan-Koch <49372504+Christiaan-Koch@users.noreply.github.com>
2021-03-18 11:52:06 +0200

    Add files via upload

---
+++ spotify-recommendation-engine/src/main/java/spotifyrecommendationengine/SpotifyRecommendationEngineApplication.java
@@ -0,0 +1,13 @@
+package spotifyrecommendationengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyRecommendationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyRecommendationEngineApplication.class, args);
+	}
+
+}
