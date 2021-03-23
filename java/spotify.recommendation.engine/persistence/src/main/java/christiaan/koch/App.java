98114e9099d6aca9cbb4c486237ed05ecd452385
ChristiaanKoch <christiaan.koch@entelect.co.za>
2021-03-23 15:57:39 +0200

    Redid java folder structure. Functional now

---
+++ java/spotify.recommendation.engine/persistence/src/main/java/christiaan/koch/App.java
@@ -0,0 +1,13 @@
+package christiaan.koch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ java/spotify.recommendation.engine/spring/src/main/java/christiaan/koch/SpotifyRecommendationEngineApplication.java
@@ -0,0 +1,11 @@
+package christiaan.koch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyRecommendationEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyRecommendationEngineApplication.class, args);
+    }
+}
